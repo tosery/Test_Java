9b46afa6e05a5dc85db02597d59a57a3f8b5f536
tosery <83147935+tosery@users.noreply.github.com>
2021-08-07 16:55:34 +0900

    javaDB20210807

---
+++ DB/No/src/DB_Book.java
@@ -0,0 +1,53 @@
+import java.sql.*;
+
+
+public class DB_Book {
+	Connection con;
+
+	DB_Book()
+	{
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String userid = "junyeong";
+		String pwd = "oracle";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("드라이버 로드 성공");
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			System.out.println("데이터베이스 연결 준비... ...");
+			con = DriverManager.getConnection(url, userid, pwd);
+			System.out.println("데이터베이스 연결 성공");
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public void sqlRun() {
+		String query = "SELECT * FROM Book";
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			System.out.println("BOOK NO \tBOOK NAME \tPUBLISHER \tPRICE");
+			while(rs.next())
+			{
+				System.out.print("\t" + rs.getInt(1));
+				System.out.print("\t" + rs.getString(2));
+				System.out.print("\t" + rs.getString(3));
+				System.out.println("\t" + rs.getInt(4));
+			}
+			con.close();
+		}catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		DB_Book db = new DB_Book();
+		db.sqlRun();
+	}
+}
+
